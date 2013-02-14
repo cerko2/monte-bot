9b569234ebd14025f67b8f7d7946e451fe4cb418
Tomas Cere <cerko2@gmail.com>
2013-02-14 17:56:00 +0100

    add Position class for vector based operations

---
+++ montyBot/src/javabot/util/Position.java
@@ -0,0 +1,60 @@
+package javabot.util;
+
+import javabot.model.Unit;
+
+public class Position {
+
+	public double x, y;
+	
+	public Position(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Position(Unit unit){
+		x = unit.getX();
+		y = unit.getY();
+	}
+	
+	public boolean equals(Position pos){
+		return x == pos.x && y == pos.y;
+	}
+	
+	public Position add(Position pos){
+		return new Position(x + pos.x, y + pos.y);
+	}
+	
+	public Position substract(Position pos){
+		return new Position(x - pos.x, y - pos.y);
+	}
+	
+	public Position multiply(double scalar){
+		return new Position(x * scalar, y * scalar);
+	}
+	
+	public Position divide(double scalar){
+		return new Position(x / scalar, y / scalar);
+	}
+	
+	public double lengthSq(){
+		return x*x + y*y;
+	}
+	
+	public double length(){
+		return Math.sqrt(lengthSq());
+	}
+	
+	public void normalize(){
+		double tmpLength = length();
+		x = x / tmpLength;
+		y = y / tmpLength;
+	}
+	
+	public void rotate(double _angle){
+		double angle = _angle * Math.PI / 180.0;
+		double tx = x * Math.cos(angle) - y * Math.sin(angle);
+		double ty = y * Math.cos(angle) + x * Math.sin(angle);
+		x = tx;
+		y = ty;
+	}
+}

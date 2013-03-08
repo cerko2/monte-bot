e0003f8933c17af952e6bef0211da714f7cf48e1
Tomas Cere <cerko2@gmail.com>
2013-03-08 01:53:54 +0100

    moved manager hierarchy from JavaBot into Boss, added getters for managers into boss

--- montyBot/src/javabot/JavaBot.java
+++ montyBot/src/javabot/JavaBot.java
@@ -22,11 +22,6 @@ public class JavaBot extends AbstractManager {
 
 	// Managers & Modules:
 	private Boss boss;
-	private WallInModule wallInModule;
-	private UnitProductionManager unitProductionManager;
-	private BuildManager buildManager;
-	
-	private Region home; // Needed only for DEBUGGING (miso certikcy)
 
 	private JNIBWAPI bwapi;
 	public static void main(String[] args) {
@@ -60,18 +55,9 @@ public class JavaBot extends AbstractManager {
 	public void initialize(){
 		// Create the managers
 		boss = new Boss(bwapi);
-		OpeningManager openingManager = new OpeningManager(bwapi);
-		wallInModule = new WallInModule(bwapi);
-		unitProductionManager = new UnitProductionManager(boss); 
-		buildManager = new  BuildManager(boss);
 		
 		// Add the managers
 		addManager(boss);
-		addManager(openingManager);
-		addManager(wallInModule);			// miso certicky
-		addManager(buildManager);			// azder
-		addManager(unitProductionManager);	// azder
-
 	}
 	
 	
@@ -83,70 +69,12 @@ public class JavaBot extends AbstractManager {
 		// Draw debug information on screen
 		drawDebugInfo();
 
-
-		// START DEBUG: Compute the wall on frame 1 (miso certicky)
-		if (bwapi.getFrameCount() == 1) {
-			home = bwapi.getMap().getRegions().get(0);
-
-			Unit homeNexus = bwapi.getMyUnits().get(0);
-			Integer dist = 999999;
-			Integer dist2;
-			for (Unit u : bwapi.getMyUnits()) {
-				if (u.getTypeID() == UnitTypes.Protoss_Nexus.ordinal()) {
-					homeNexus = u;
-					break;
-				}
-			}
-			for (Region r : bwapi.getMap().getRegions()) {
-				dist2 = Math.abs(r.getCenterX()-homeNexus.getX()) + Math.abs(r.getCenterY()-homeNexus.getY()); 
-				if (dist2 < dist) {
-					dist = dist2;
-					home = r;
-				}
-			}
-
-			for (ChokePoint c : home.getChokePoints()) {
-				wallInModule.computeWall( c, home, UnitTypes.Zerg_Zergling.ordinal());
-			}
-		}
-		// END DEBUG
-
 	}
 	
 	// Draws debug information on the screen. 
 	// Reimplement this function however you want. 
 	public void drawDebugInfo() {
 
-		// Draw our home position and chokes
-		if (home != null) {
-			bwapi.drawText(new Point(5,0), "Our home position: "+String.valueOf(home.getCenterX())+","+String.valueOf(home.getCenterY()), true);
-			bwapi.drawText(new Point(5,14), "Map: "+bwapi.getMap().getName(), true);
-
-			// chokepoints and center of the home Region
-			bwapi.drawCircle(home.getCenterX(), home.getCenterY(), 15, BWColor.TEAL, true, false);
-			for (ChokePoint c : home.getChokePoints()) {
-				bwapi.drawLine(new Point(home.getCenterX() , home.getCenterY() ), new Point(c.getCenterX(),c.getCenterY()),BWColor.TEAL,false);
-				bwapi.drawCircle(c.getCenterX(),c.getCenterY(), 10, BWColor.TEAL, true, false);
-			}
-		}
-
-		for (int i=1; i < bwapi.getMap().getWidth(); i++) {
-			for (int j=1; j < bwapi.getMap().getHeight(); j++) {
-				if (wallInModule.obstructedByNeutrals(i, j))
-					bwapi.drawCircle(i*32,j*32, 5, BWColor.GREEN, false, false);
-			}
-		}
-		
-		// Draw all previously computed walls
-		for (Wall w : wallInModule.getAllWalls()) {
-			for (Point bt : w.getBuildTiles()) {
-				Integer tileWidth = bwapi.getUnitType(w.getBuildingTypeIds().get(w.getBuildTiles().indexOf(bt))).getTileWidth();
-				Integer tileHeight = bwapi.getUnitType(w.getBuildingTypeIds().get(w.getBuildTiles().indexOf(bt))).getTileHeight();
-				bwapi.drawBox(bt.x*32, bt.y*32, (bt.x + tileWidth)*32, (bt.y + tileHeight)*32, BWColor.YELLOW, false, false);
-				bwapi.drawText(new Point(bt.x*32+4, bt.y*32+2), bwapi.getUnitType(w.getBuildingTypeIds().get(w.getBuildTiles().indexOf(bt))).getName()+" "+String.valueOf(bt.x)+","+String.valueOf(bt.y), false);
-			}
-		}
-		
 		//Static unit debugging
 		if (STATIC_UNIT_DEBUG){
 			for (Unit unit : bwapi.getAllStaticNeutralUnits()){
--- montyBot/src/javabot/macro/Boss.java
+++ montyBot/src/javabot/macro/Boss.java
@@ -1,25 +1,42 @@
 package javabot.macro;
 
+import java.awt.Point;
 import java.util.ArrayList;
 
 import javabot.AbstractManager;
 import javabot.JNIBWAPI;
 import javabot.combat.MonteCarloPlanner;
 import javabot.combat.micro.ScoutingManager;
+import javabot.model.ChokePoint;
 import javabot.model.Player;
+import javabot.model.Region;
 import javabot.model.Unit;
+import javabot.strategy.OpeningManager;
+import javabot.strategy.WallInModule;
 import javabot.types.UnitType.UnitTypes;
+import javabot.util.BWColor;
+import javabot.util.Wall;
 
 public class Boss extends AbstractManager{
+	
+	public static final boolean BOSS_DEBUG = true;
+	public static final boolean WALLIN_DEBUG = true;
+	
+	private Region home; // Needed only for DEBUGGING (miso certikcy)
 	
 	public JNIBWAPI game;
 	private Player player;
 	
 	private final int workerDefenseTreshold = 3;
 	
+	// Managers & Modules:
+	private BuildManager buildManager;
 	private MonteCarloPlanner montePlanner;
-	private WorkerManager workerManager;
+	private OpeningManager openingManager;
 	private ScoutingManager scoutManager;
+	private UnitProductionManager unitProductionManager;
+	private WallInModule wallInModule;
+	private WorkerManager workerManager;
 	
 	private ArrayList<Unit> validUnits;
 	private ArrayList<Unit> combatUnits;
@@ -39,7 +56,7 @@ public class Boss extends AbstractManager{
 	
 	public Boss (JNIBWAPI game){
 		this.game = game;
-		this.player = game.getSelf(); // azder
+		this.player = game.getSelf();
 	}
 	
 	public void initialize(){
@@ -51,10 +68,23 @@ public class Boss extends AbstractManager{
 		scoutUnits = new ArrayList<Unit>();
 		workerUnits = new ArrayList<Unit>();
 		assignedUnits = new ArrayList<Unit>();
+		
+		//subordinate initialization
+		openingManager = new OpeningManager(game);
+		wallInModule = new WallInModule(game);
+		unitProductionManager = new UnitProductionManager(this); 
+		buildManager = new  BuildManager(this);
+		
+		addManager(openingManager);
+		addManager(wallInModule);			// miso certicky
+		addManager(buildManager);			// azder
+		addManager(unitProductionManager);	// azder
 	}
 	
 	public void gameStarted(){
 		initialize();
+		
+		super.gameStarted();
 	}
 	
 	public void gameUpdate(){
@@ -62,10 +92,14 @@ public class Boss extends AbstractManager{
 		montePlanner.update(combatUnits);
 		workerManager.update(workerUnits);
 		
-		if(player != null){ //azder
-			minerals = player.getMinerals();
-			gas = player.getGas();
+		minerals = player.getMinerals();
+		gas = player.getGas();
+		
+		if (BOSS_DEBUG){
+			debug();
 		}
+		
+		super.gameUpdate();
 	}
 	
 	private void setUnits(){
@@ -84,6 +118,8 @@ public class Boss extends AbstractManager{
 				validUnits.add(unit);
 			}
 		}
+		
+		divideResources();
 	}
 	
 	private boolean isValidUnit(Unit unit){
@@ -193,4 +229,97 @@ public class Boss extends AbstractManager{
 		return false;
 	}
 	
+	private void divideResources(){
+		
+	}
+	
+	public BuildManager getBuildManager() {
+		return buildManager;
+	}
+
+	public MonteCarloPlanner getMontePlanner() {
+		return montePlanner;
+	}
+
+	public OpeningManager getOpeningManager() {
+		return openingManager;
+	}
+
+	public ScoutingManager getScoutManager() {
+		return scoutManager;
+	}
+
+	public UnitProductionManager getUnitProductionManager() {
+		return unitProductionManager;
+	}
+
+	public WallInModule getWallInModule() {
+		return wallInModule;
+	}
+
+	public WorkerManager getWorkerManager() {
+		return workerManager;
+	}
+	
+	private void debug(){
+		
+		if (WALLIN_DEBUG){
+			// START DEBUG: Compute the wall on frame 1 (miso certicky)
+			if (game.getFrameCount() == 1) {
+				home = game.getMap().getRegions().get(0);
+
+				Unit homeNexus = game.getMyUnits().get(0);
+				Integer dist = 999999;
+				Integer dist2;
+				for (Unit u : game.getMyUnits()) {
+					if (u.getTypeID() == UnitTypes.Protoss_Nexus.ordinal()) {
+						homeNexus = u;
+						break;
+					}
+				}
+				for (Region r : game.getMap().getRegions()) {
+					dist2 = Math.abs(r.getCenterX()-homeNexus.getX()) + Math.abs(r.getCenterY()-homeNexus.getY()); 
+					if (dist2 < dist) {
+						dist = dist2;
+						home = r;
+					}
+				}
+
+				for (ChokePoint c : home.getChokePoints()) {
+					wallInModule.computeWall( c, home, UnitTypes.Zerg_Zergling.ordinal());
+				}
+			}
+			// END DEBUG
+
+			// Draw our home position and chokes
+			if (home != null) {
+				game.drawText(new Point(5,0), "Our home position: "+String.valueOf(home.getCenterX())+","+String.valueOf(home.getCenterY()), true);
+				game.drawText(new Point(5,14), "Map: "+game.getMap().getName(), true);
+
+				// chokepoints and center of the home Region
+				game.drawCircle(home.getCenterX(), home.getCenterY(), 15, BWColor.TEAL, true, false);
+				for (ChokePoint c : home.getChokePoints()) {
+					game.drawLine(new Point(home.getCenterX() , home.getCenterY() ), new Point(c.getCenterX(),c.getCenterY()),BWColor.TEAL,false);
+					game.drawCircle(c.getCenterX(),c.getCenterY(), 10, BWColor.TEAL, true, false);
+				}
+			}
+
+			for (int i=1; i < game.getMap().getWidth(); i++) {
+				for (int j=1; j < game.getMap().getHeight(); j++) {
+					if (wallInModule.obstructedByNeutrals(i, j))
+						game.drawCircle(i*32,j*32, 5, BWColor.GREEN, false, false);
+				}
+			}
+
+			// Draw all previously computed walls
+			for (Wall w : wallInModule.getAllWalls()) {
+				for (Point bt : w.getBuildTiles()) {
+					Integer tileWidth = game.getUnitType(w.getBuildingTypeIds().get(w.getBuildTiles().indexOf(bt))).getTileWidth();
+					Integer tileHeight = game.getUnitType(w.getBuildingTypeIds().get(w.getBuildTiles().indexOf(bt))).getTileHeight();
+					game.drawBox(bt.x*32, bt.y*32, (bt.x + tileWidth)*32, (bt.y + tileHeight)*32, BWColor.YELLOW, false, false);
+					game.drawText(new Point(bt.x*32+4, bt.y*32+2), game.getUnitType(w.getBuildingTypeIds().get(w.getBuildTiles().indexOf(bt))).getName()+" "+String.valueOf(bt.x)+","+String.valueOf(bt.y), false);
+				}
+			}
+		}
+	}
 }

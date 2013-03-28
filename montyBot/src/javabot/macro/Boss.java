3eb70fc981f60b60a2465e9a5e1e95d9844d05a8
Janko <Janko@Janko-NBK>
2013-03-28 14:21:10 +0100

    initial commit

--- montyBot/src/javabot/macro/Boss.java
+++ montyBot/src/javabot/macro/Boss.java
@@ -17,6 +17,7 @@ import javabot.model.Region;
 import javabot.model.Unit;
 import javabot.strategy.ArmyCompositionManager;
 import javabot.strategy.OpeningManager;
+import javabot.strategy.OpponentKnowledgeBase;
 import javabot.strategy.OpponentModeling;
 import javabot.strategy.OpponentPositioning;
 import javabot.strategy.WallInModule;
@@ -48,6 +49,7 @@ public class Boss extends AbstractManager{
 	private BuildManager buildManager;
 	private MonteCarloPlanner montePlanner;
 	private OpeningManager openingManager;
+	private OpponentKnowledgeBase opponentKnowledgeBase;
 	private OpponentModeling opponentModeling;
 	private OpponentPositioning opponentPositioning;
 	private ScoutingManager scoutManager;
@@ -99,6 +101,7 @@ public class Boss extends AbstractManager{
 		//subordinate initialization
 		buildManager = new  BuildManager(this);
 		montePlanner = new MonteCarloPlanner();
+		opponentKnowledgeBase = new OpponentKnowledgeBase(this);
 		openingManager = new OpeningManager(this);
 		opponentPositioning = new OpponentPositioning(game);
 		scoutManager = new ScoutingManager(game);
@@ -109,6 +112,7 @@ public class Boss extends AbstractManager{
 		opponentModeling = new OpponentModeling(game, opponentPositioning);
 		armyCompositionManager = new ArmyCompositionManager(game, unitProductionManager, opponentPositioning, opponentModeling);
 		
+		addManager(opponentKnowledgeBase);
 		addManager(openingManager);
 		addManager(opponentPositioning);
 		addManager(wallInModule);			// miso certicky
--- montyBot/src/javabot/strategy/OpponentKnowledgeBase.java
+++ montyBot/src/javabot/strategy/OpponentKnowledgeBase.java
@@ -1,5 +1,65 @@
 package javabot.strategy;
 
-public class OpponentKnowledgeBase {
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Scanner;
 
+import javabot.AbstractManager;
+import javabot.JNIBWAPI;
+import javabot.macro.Boss;
+
+public class OpponentKnowledgeBase extends AbstractManager{
+	
+	private JNIBWAPI game;
+	private Boss boss;
+	
+	private String enemyName = "";
+	private int openingID = -1;
+	
+	private HashMap<String, HashMap<Integer, double[]>> knowledgeBase;
+	
+	public OpponentKnowledgeBase(Boss boss){
+		this.game = boss.game;
+		this.boss = boss;
+		enemyName = game.getEnemies().get(0).getName();
+		knowledgeBase = new HashMap<String, HashMap<Integer, double[]>>();
+		try {
+			readFile();
+		} catch (IOException e) {
+			System.err.println("Error while reading opponentKnowledgeBase.txt");
+			e.printStackTrace();
+		}
+	}
+	
+	public void setOpeningID(int ID){
+		openingID = ID;
+	}
+	
+	public void gameEnded(){
+		if (openingID != -1){
+			
+		}
+	}
+	
+	private void readFile() throws IOException {
+		Path path = Paths.get("bwapi-data/AI/montyBot/opponentKnowledgeBase.txt");
+	    try (Scanner scanner =  new Scanner(path, StandardCharsets.UTF_8.name())){
+	    	if (scanner.hasNextLine()){
+	    		int numberOfRecords = scanner.nextInt();
+	    		scanner.nextLine();
+	    		for (int i = 1; i <= numberOfRecords; i++){
+	    			String name = scanner.nextLine();
+	    			while (scanner.hasNextLine() && !scanner.nextLine().equals("#PLAYER")){
+	    				//TODO read openings
+	    			}
+	    		}
+	    	}
+	    	else{
+	    		game.printText("Empty knowledge base.");
+	    	}
+	    }
+	}
 }

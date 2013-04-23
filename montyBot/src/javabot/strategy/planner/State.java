0bcafee3da871bf0b81f786dfb57f45a4b7a3ddf
Tomas Cere <cerko2@gmail.com>
2013-04-23 02:04:14 +0200

    - changed planner package layout

---
+++ montyBot/src/javabot/strategy/planner/State.java
@@ -0,0 +1,12 @@
+package javabot.strategy.planner;
+
+import javabot.strategy.planner.action.Action;
+
+public class State {
+	
+	private Action action;
+	private int minerals;
+	private int gas;
+	private int supplyUsed;
+
+}

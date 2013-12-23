c9de620549bd00abef99f549d043e192f72dfc39
Tomas <thomas.vajda@gmail.com>
2013-12-23 03:26:42 +0100

    Worker Defense manager update - workers self defense created

--- montyBot/src/javabot/combat/WorkerDefenseManager.java
+++ montyBot/src/javabot/combat/WorkerDefenseManager.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 import javabot.JNIBWAPI;
 import javabot.macro.Boss;
 import javabot.model.Unit;
+import javabot.types.OrderType.OrderTypeTypes;
+import javabot.types.UnitType;
+import javabot.types.UnitType.UnitTypes;
+import javabot.util.Position;
 
 
 public class WorkerDefenseManager {
@@ -29,8 +33,39 @@ public class WorkerDefenseManager {
 	 * @param units - units assigned to worker defense manager
 	 */
 	public void update(ArrayList<Unit> units) {
-		
-		
+		doWorkersSelfDefense(units);
+	}
+	
+	private void doWorkersSelfDefense(ArrayList<Unit> units) {
+		for (Unit unit: units) {
+			if (unit.getTypeID() == UnitTypes.Protoss_Probe.ordinal()) {
+				boolean stopAttack = true;
+
+				Position unitPosition = new Position(unit.getX(), unit.getY());
+				
+				ArrayList<Unit> enemyUnits = game.getEnemyUnits();
+				
+				for (Unit enemyUnit: enemyUnits) {
+					UnitType enemyUnitType = game.getUnitType(unit.getTypeID());
+
+					if (enemyUnitType != null && unitPosition.distance(new Position(enemyUnit.getX(), enemyUnit.getY())) < 48) {
+						if (enemyUnitType.isCanAttackGround() && !enemyUnitType.isFlyer()) {
+							if (enemyUnit.getOrderID() != OrderTypeTypes.PlaceBuilding.ordinal()) {
+								game.attack(unit.getID(), enemyUnit.getID());
+							}
+						}
+						stopAttack = false;
+						break;
+					}
+				}
+		      
+				stopAttack = true;
+
+				if (unit.getOrderID() == OrderTypeTypes.AttackUnit.ordinal() && stopAttack) {
+					game.stop(unit.getID());
+				}
+		    }
+		}
 	}
 	
 }
